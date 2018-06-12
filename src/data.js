cdc65d66d5da08cecd765203898a703e4de3ea70
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-06-11 21:51:09 -0500

    Realizando la primera Funcion sortUsers(users, orderBy, orderDirection)

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
-const conexion = (url)=> {
+const conexion = (url) => {
   let xhr = new XMLHttpRequest();
   //obtenemos la data de usurios con l url absoluta
   xhr.open('GET', url);
   //hicimos una solicitud asincrona
-  xhr.onload = function(){
-    if(xhr.status >= 200 && xhr.status < 400){
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 400) {
       let dataUser = JSON.parse(xhr.responseText);
       console.log(dataUser);
     }
@@ -16,6 +16,40 @@ const conexion = (url)=> {
 conexion('/data/cohorts/lim-2018-03-pre-core-pw/users.json');
 conexion('/data/cohorts/lim-2018-03-pre-core-pw/progress.json');
 conexion('/data/cohorts.json');
+
+
+00Hjv4mcbsdjkdshkdshjkds.intro.totalDuration
+00Hjv4mcbsdjkdshkdshjkds.intro.units["01-introduction"].percent
+00Hjv4mcbsdjkdshkdshjkds.intro.units["01-introduction"].parts
+
+
+
+window.computeUsersStats = (users, progress, courses) => {
+  //Creamos un Objeto con la proipiedad stats
+  let usersWithStats = {
+    //Objeto stats
+    stats: {
+      percent: 23,
+      exercises: {
+        total: 23,
+        completed: 23,
+        percent : 23
+      },
+      reads: {
+        total:23,
+        completed:23,
+        percent:23
+      },
+      quizzes: {
+        total : 23,
+        completed:23,
+        scoreSum: 23,
+        scoreAvg: 23
+      },
+    }
+
+  };
+};
 
 
 
