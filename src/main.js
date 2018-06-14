09662bbdcbb903e4d3be8dae28b81da880ab3175
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-06-13 21:09:28 -0500

    Vinculamos la data Ususer y lo mostramos en el HTML

--- src/main.js
+++ src/main.js
@@ -1,37 +1,40 @@
-const urlUser = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
 const btnUser = document.getElementById('btnMostrarUser');
 
-// Agregamos un evento submit y las instrucciones a ejecutar
-btnUser.addEventListener('click', function (e) {
+const urlUser = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
+
+const getJSON = (url, callback) =>{
+  //Creamos nuestro Objeto
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  //La funcion onload se le asigna la funcion addNEws
+  request.onload = callback;
+  //La funcion onerror tiene asignado la funcion handleError 
+  request.onerror = handleError;
+  request.send();
+}
+
+const handleError = () =>{
+  console.log('Se ha prespepitoentado un error');
+}
+
+const addUsers = () => {
+  //debugger
+  const data = JSON.parse(event.currentTarget.responseText);
+  
+  data.map((usuario)=>{
+   // console.log(usuario.name + '//' + usuario.signupCohort );
+    document.getElementById('response-container').innerHTML += '<li>' + usuario.name + '</li>';
+  });  
+ //console.log(data);
+ }
+
+ 
+
+  // Agregamos un evento submit y las instrucciones a ejecutar
+btnUser.addEventListener('click', e => {
   e.preventDefault();
-  getNews();
+  getJSON(urlUser, addUser);
+  
 });
 
 
-function getNews() {
-  //Creamos nuestro Objeto
-  const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-  //La funcion onload se le asigna la funcion addNEws
-  articleRequest.onload = addNews;
-  //La funcion onerror tiene asignado la funcion handleError 
-  articleRequest.onerror = handleError;
-  articleRequest.send();
-}
-
-function handleError() {
-  console.log('Se ha presentado un error');
-}
-
-function addNews() {
-  const data = JSON.parse(this.responseText);
-  data.map((usuario)=>{
-    console.log(usuario.name );
-    //document.getElementById('response-container').innerHTML=usuario.name;
-  });
-
-   
-  //console.log(data);
- }
-
-  
