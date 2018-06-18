05369e13cb005a4a085fc1c74c02cc15a74df8f3
ticze <ticze2598@gmail.com>
2018-06-18 16:30:08 -0500

    creando la funcion isUser y progreso

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ const selectbtn = document.getElementById('select-cohorts');
 const listUsers = document.getElementById('container-user');
 const urlUser = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const urlCohorts = '../data/cohorts.json';
-const urlProgress = '';
+const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
 const getJSON = (url, callback) => {
   //Creamos nuestro Objeto
   const request = new XMLHttpRequest();
   request.open('GET', url);
-  //La funcion onload se le asigna la funcion addNEws
+  //La funcion onload se le asigna la funcion callback
   request.onload = callback;
   //La funcion onerror tiene asignado la funcion handleError 
   request.onerror = handleError;
@@ -20,25 +20,37 @@ const handleError = () => {
   console.log('Se ha presentado un error');
 }
 //FUNCION DE LISTA DE USUARIO
- const addUsers = () => { 
-  //debugger
-  const data = JSON.parse(event.currentTarget.responseText);
-
-   data.map((usuario) => {
-      let listUser = document.createElement('li');
-      listUser.innerHTML = usuario.name;
-      listUsers.appendChild(listUser);
-    }); 
-
-}
-
-//FUNCION LISTA DE COHORTS
-const addCohorts = (event) => {
+const addUsers = (event) => { 
   //debugger
   const data = JSON.parse(event.target.responseText);
-
+  data.map((usuario) => {
+    let listUser = document.createElement('li');
+    listUser.innerHTML = usuario.name;
+    listUsers.appendChild(listUser);
+  }); 
+}
+//FUNCION DE ID DE USUARIO
+const idUser = (event) => {
+  const data = JSON.parse(event.target.responseText);
+  data.map((usuario)=>{
+    let listId = document.createElement('li');
+    listId.innerHTML = usuario.id;
+    listId.appendChild(listId);
+  });
+}
+//FUNCION DEL PROGRESO
+const addProgress = (event) => {
+  const data = JSON.parse(event.target.responseText);
+  data.map((progress)=>{
+    let listProgress = document.createElement('li');
+    listProgress.innerHTML = progress.intro;
+    listProgress.appendChild(listProgress);
+  }); 
+}
+//FUNCION LISTA DE COHORTS
+const addCohorts = (event) => {
+  const data = JSON.parse(event.target.responseText);
   data.map((cohorts) => {
-
     let listCor = document.createElement('option');
     listCor.value = cohorts.id;
     listCor.innerHTML = cohorts.id;
@@ -46,7 +58,7 @@ const addCohorts = (event) => {
   });
 
 }
-
+//
 selectbtn.addEventListener('change', e => {
   e.preventDefault();
   if(selectbtn.value === 'lim-2018-03-pre-core-pw') {
