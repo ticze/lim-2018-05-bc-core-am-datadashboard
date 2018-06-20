12b50f5558f515b8d3034388ad45257872402c2c
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-06-20 00:16:25 -0500

    Mostrando las dos datos

--- src/data.js
+++ src/data.js
@@ -1,43 +1,76 @@
 //CREAANDO LAS  FUNCIONES
 
-window.computeUsersStats = (users, progress, courses) => { 
+window.computeUsersStats = (users, progress, courses) => {
 
   const keyAddProgress = Object.keys(progress);
-  
+
   for (const usuario of users) {
-    keyAddProgress.map((codigoUser) =>{
-      if (usuario.id === codigoUser) {       
-        if (progress[codigoUser].intro !== undefined ){
-          
-          let usersWithStats = {            
-            //Objeto stats
+    //Vamos a Mapear el ARRAY PROGRESS
+    keyAddProgress.map((codigoUser) => {
+      if (usuario.id === codigoUser) {
+        if (progress[codigoUser].intro !== undefined) {
+
+          console.log(usuario.name);
+          //Un Arreglo 
+          const usersWithStats = {//Objeto stats              
             stats: {
               percent: progress[codigoUser].intro.percent,
+              exercises: {
+                total: progress[codigoUser].intro.totalParts,
+                completed:23,
+                percent:23,
+              },
+              reads: {
+                total: 23,
+                completed:23,
+                percent:23,
+              },
+              quizzes: {
+                total: 23,
+                completed:23,
+                percent:23,
+              }
             }
-               
           };
-
           console.log(usersWithStats);
-          
-            
-              
+
         }
-      } 
+      }
     });
-  
+
 
 
   }
-  
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Funcion para Ordenar
-window.sortUsers = (users, orderBy, orderDirection)=>{
+window.sortUsers = (users, orderBy, orderDirection) => {
 
 };
 
 
-window.filterUsers = (users, search)=>{
+window.filterUsers = (users, search) => {
 
 }
 
