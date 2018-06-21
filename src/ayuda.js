16106e1233c79cf7a4a055d19fea08585478f2ad
ticze <ticze2598@gmail.com>
2018-06-21 00:26:13 -0500

    retornando nombres de usuarios , percent ,y los coursesIndex en consola

--- src/ayuda.js
+++ src/ayuda.js
@@ -15,38 +15,21 @@ const handleError = () => {
 }
 
 const addUserProgress = () => {
-  const courses = JSON.parse(event.target.responseText);
-
   const users = JSON.parse(event.target.responseText);
   
-  const progress = (event) => {
-    const progress = JSON.parse(event.target.responseText);
-   
-    computeUsersStats(users,progress,courses);
+  const addCohort = () => {
+    const courses = JSON.parse(event.target.responseText);
 
+    const progress = (event) => {
+      const progress = JSON.parse(event.target.responseText);
+     
+      computeUsersStats(users,progress,courses);
+  
+    }
+    getJSON(urlProgress, progress);
   }
-  getJSON(urlProgress, progress);
-  getJSON(urlCohorts, courses);
+  getJSON(urlCohorts, addCohort);
 }
 getJSON(urlUser, addUserProgress);
 
 
-/* //FUNCION LISTA DE COHORTS
-const addCohorts = (event) => {
-  const data = JSON.parse(event.target.responseText);
-  data.map((cohorts) => {
-    let listCor = document.createElement('option');
-    listCor.value = cohorts.id;
-    listCor.innerHTML = cohorts.id;
-    selectbtn.appendChild(listCor);
-  });
-}
-
-selectbtn.addEventListener('change', e => {
-  e.preventDefault();
-  if(selectbtn.value === 'lim-2018-03-pre-core-pw') {
-    getJSON(urlUser,addUsers);
-  }   
-});
-getJSON(urlCohorts, addCohorts) 
- */
--- src/data.js
+++ src/data.js
@@ -1,24 +1,41 @@
-window.computeUsersStats = (users, progress, courses) => { 
-
+window.computeUsersStats = (users, progress, courses) => {
   const keyAddProgress = Object.keys(progress);
-  
-  for (const usuario of users) {
-    keyAddProgress.map((codigoUser) =>{
-      if (usuario.id === codigoUser) {       
-        if (progress[codigoUser].intro !== undefined ){
-          
-          let usersWithStats = {            
-            //Objeto stats
-            stats: {
-              percent: progress[codigoUser].intro.percent,
+  for (const cursos of courses) {
+    for (const usuario of users) {
+      keyAddProgress.map((codigoUser) => {
+        if (usuario.id === codigoUser) {
+          if (progress[codigoUser].intro !== undefined) {
 
-            }
-          };
-          //return(keyAddProgress)
-          console.log(usersWithStats);    
+            let name = usuario.name;
+                 
+            let usersWithStats = {
+              stats: {
+                percent: progress[codigoUser].intro.percent,
+
+                exercises : {
+                  total: 5,
+                  completed: 5,
+                  percent: 7
+                },
+                reads: {
+                  total : 5,
+                  completed : 4,
+                  percent : 2
+                },
+                quiz :{
+                  total : 5,
+                  completed : 5,
+                  percent : 2,
+                  scoreSum : 2,
+                  scoreAvg : 5
+                }
+              }
+            };
+            let cursosCohort = cursos.coursesInd;
+          }
         }
-      } 
-    });
+      });
+    }
   }
 };
 
